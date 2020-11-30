da1b95274993dd0190d434e93e7afe4f78a378c4
Felix Antonio Sirit Murillo <siritfelix@ibm.com>
2020-11-30 11:53:59 -0300

    Primer commit

---
+++ src/test/java/ar/com/meli/quasar/QuasarApplicationTests.java
@@ -0,0 +1,13 @@
+package ar.com.meli.quasar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuasarApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
